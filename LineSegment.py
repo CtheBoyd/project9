270fd3520101ee1c8bb9a0b7760cf62fcbe0c239
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-05-25 21:59:14 -0700

    update

--- LineSegment.py
+++ LineSegment.py
@@ -64,13 +64,13 @@ point_1 = Point(7, 4)
 
 point_2 = Point(-6, 18)
 
-print(point_1.distance_to(point_2))
+#print(point_1.distance_to(point_2))
 
 line_seg_1 = LineSegment(point_1, point_2)
 
-print(line_seg_1.length())
+#print(line_seg_1.length())
 
-print(line_seg_1.slope())
+#print(line_seg_1.slope())
 
 point_3 = Point(-2, 2)
 
@@ -78,8 +78,8 @@ point_4 = Point(24, 12)
 
 line_seg_2 = LineSegment(point_3, point_4)
 
-print(line_seg_2.length())
+#print(line_seg_2.length())
 
-print(line_seg_2.slope())
+#print(line_seg_2.slope())
 
-print(line_seg_1.is_parallel_to(line_seg_2))
+#print(line_seg_1.is_parallel_to(line_seg_2))

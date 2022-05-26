2c6e6e81e6958b0ff61ba6fce084477c2e50549d
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-05-25 21:56:47 -0700

    update

--- LineSegment.py
+++ LineSegment.py
@@ -59,3 +59,27 @@ class LineSegment:
         """returns the slope of each line"""
         return (self.endpoint_2.get_y_coord() - self.endpoint_1.get_y_coord()) / (
                 self.endpoint_2.get_x_coord() - self.endpoint_1.get_x_coord())
+
+point_1 = Point(7, 4)
+
+point_2 = Point(-6, 18)
+
+print(point_1.distance_to(point_2))
+
+line_seg_1 = LineSegment(point_1, point_2)
+
+print(line_seg_1.length())
+
+print(line_seg_1.slope())
+
+point_3 = Point(-2, 2)
+
+point_4 = Point(24, 12)
+
+line_seg_2 = LineSegment(point_3, point_4)
+
+print(line_seg_2.length())
+
+print(line_seg_2.slope())
+
+print(line_seg_1.is_parallel_to(line_seg_2))
